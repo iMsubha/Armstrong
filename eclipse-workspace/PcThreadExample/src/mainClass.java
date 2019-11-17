bd94eed626549661c01d3b59347704b50d58269d
Subha Fairuz <iMsubha>
2019-11-18 02:49:00 +0600

    1st commit

---
+++ eclipse-workspace/PcThreadExample/src/mainClass.java
@@ -0,0 +1,11 @@
+public class Intbuffer
+{
+	private int index;
+	private int[] buffer= new int[8];
+}
+
+public class mainClass {
+	public static void main(String[] arg) {
+		Intbuffer b = new Intbuffer();
+	}
+}
